7061a9413ca6dcf77eddae59ecf0cd79ca2ff4c4
WHT <329335515@qq.com>
2024-08-29 11:13:13 +0800

    GuliMail Frist Commit

---
+++ GuliMail/src/main/java/com/guigu/gulimail/GuliMailApplication.java
@@ -0,0 +1,13 @@
+package com.guigu.gulimail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliMailApplication.class, args);
+    }
+
+}
---
+++ GuliMail/src/test/java/com/guigu/gulimail/GuliMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guigu.gulimail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
